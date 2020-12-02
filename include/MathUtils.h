0cb6ff4133b6848b8908903ab6d5ff1f20260556
Louis Cianciullo <louis.cianciullo@my.jcu.edu.au>
2020-12-02 21:29:35 +0930

    refactor: changed to lowercase for the start of function names, to match the convention of my other classes.

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -14,8 +14,8 @@ public:
 	T getX() const { return _xy[0]; }
 	T getY() const { return _xy[1]; }
 
-	void SetX(const T x) { _xy[0] = x; }
-	void SetY(const T y) { _xy[1] = y; }
+	void setX(const T x) { _xy[0] = x; }
+	void setY(const T y) { _xy[1] = y; }
 	
 	Vec2 operator+ (const Vec2& vec) const {
 		return Vec3(
@@ -56,24 +56,24 @@ public:
 		return _xy == vec._xy;
 	}
 
-	Vec2& Normalise() {
-		T ln = Length();
+	Vec2& normalise() {
+		T ln = length();
 		if (ln > 0)
 			*this /= len
 			return *this;
 	}
-	T Length() const {
+	T length() const {
 		return(std::sqrt(_xy[0] * _xy[0] + _xy[1] * _xy[1]));
 	}
-	T Dot(const Vec2& vec) {
+	T dot(const Vec2& vec) {
 		return _xy[0] * vec._xy[0] + _xy[1] * vec._xy[1];
 	}
-	T Distance(const Vec2& vec) const {
+	T distance(const Vec2& vec) const {
 		dist_x = _xy[0] - vec._xy[0];
 		dist_y = _xy[1] - vec._xy[1];
 		return std::sqrt(dist_x * dist_x + dist_y * dist_y);
 	}
-	T Cross(const Vec2& vec) const {
+	T cross(const Vec2& vec) const {
 		return _xy[0] * vec._xy[1] - _xy[1] * vec._xy[0];
 	}
 	
@@ -95,9 +95,9 @@ public:
 	T getY() const { return _xyz[1]; }
 	T getZ() const { return _xyz[2]; }
 
-	void SetX(const T x) { _xyz[0] = x; }
-	void SetY(const T y) { _xyz[1] = y; }
-	void SetZ(const T z) { _xyz[2] = z; }
+	void setX(const T x) { _xyz[0] = x; }
+	void setY(const T y) { _xyz[1] = y; }
+	void setZ(const T z) { _xyz[2] = z; }
 
 	Vec3 operator+ (const Vec3& vec) const {
 		return Vec3(
@@ -145,25 +145,25 @@ public:
 		return _xyz == vec._xyz;
 	}
 
-	Vec3& Normalise() {
-		T ln = Length();
+	Vec3& normalise() {
+		T ln = length();
 		if (ln > 0)
 			*this /= len
 			return *this;
 	}
-	T Length() const {
+	T length() const {
 		return(std::sqrt(_xyz[0] * _xyz[0] + _xyz[1] * _xyz[1] + _xyz[2] * _xyz[2]));
 	}
-	T Dot(const Vec3& vec) const {
+	T dot(const Vec3& vec) const {
 		return _xyz[0] * vec._xyz[0] + _xyz[1] * vec._xyz[1] + _xyz[2] * vec._xyz[2];
 	}
-	T Distance(const Vec3& vec) const {
+	T distance(const Vec3& vec) const {
 		dist_x = _xyz[0] - vec._xyz[0];
 		dist_y = _xyz[1] - vec._xyz[1];
 		dist_z = _xyz[2] - vec._xyz[2];
 		return std::sqrt(dist_x * dist_x + dist_y * dist_y + dist_z + dist_z);
 	}
-	Vec3 Cross(const Vec3& vec) const {
+	Vec3 cross(const Vec3& vec) const {
 		return Vec3(
 			_xyz[1] * vec._xyz[2] - _xyz[2] * vec._xyz[1],
 			_xyz[3] * vec._xyz[1] - _xyz[1] * vec._xyz[3],
